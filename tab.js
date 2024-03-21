f17315085d2679ce444cb1cdd400079e9a21b408
heejun <yhj2512@naver.com>
2024-03-21 17:34:57 +0900

    240321

--- tab.js
+++ tab.js
@@ -63,3 +63,51 @@ if($("#closet-select option:selected").val() == 'hat'){
 }
 });
 
+var 출석부 = ['흥민', '영희', '철수', '재석'];
+
+function 이름찾기(a){
+  var 출석부 = ['흥민', '영희', '철수', '재석'];
+
+  //a가 출석부 안에 있으면 있어요, 없으면 공백
+  for(let i = 0; i < 출석부.length; i++){
+    if(출석부[i] == a){
+      return '있어요'; 
+    }
+
+  }
+  return '  ';
+}
+function 구구단(){
+for(i=2; i<=9; i++){
+  console.log(
+    1*i, 2*i, 3*i, 4*i, 5*i, 6*i, 7*i, 8*i, 9*i
+    )
+
+}}
+
+
+eleven_성적 = '80'
+
+function 성적확인(c){
+var 모의고사성적 = [62, 73, 92, 74, 91, 77]
+var a = 0;
+var b = 0;
+for(let i=0; i<모의고사성적.length; i++){
+  a = a + 모의고사성적[i];
+  
+  console.log(a)
+}
+
+var b = a/모의고사성적.length
+console.log(b)
+
+if(b>c){
+  return "성적이 "+(b-c)+"만큼 낮네요"
+} else if(b==c){
+  return '성적이 같네요'
+} else {
+  return '성적이 '+(c-b)+'만큼 높네요'
+}
+
+}
+
