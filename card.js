69f0d511a7e86ce575e26d77dec17492dbf620f3
heejun <yhj2512@naver.com>
2024-03-27 17:26:09 +0900

    0327

--- card.js
+++ card.js
@@ -11,7 +11,7 @@ var products = [
     `<div class="col-sm-4">
       <img src="https://via.placeholder.com/600" class="w-100">
       <h5>${products[i].title}</h5>
-      <p>가격 : ${products[i].price}</p>
+      <p style = "margin-right : 5px" class="수평정렬 cost_btn">가격 : ${products[i].price}</p><div class="수평정렬 btn btn-danger buy_btn">구매</div>
     </div>`;
     $('#card').append(템플릿)
 });
@@ -29,7 +29,7 @@ products.forEach((a, i)=>{
   `<div class="col-sm-4">
     <img src="https://via.placeholder.com/600" class="w-100">
     <h5>${products[i].title}</h5>
-    <p>가격 : ${products[i].price}</p>
+    <p style = "margin-right : 5px" class="수평정렬 cost_btn">가격 : ${products[i].price}</p><div class="수평정렬 btn btn-danger" buy_btn">구매</div>
   </div>`;
   $('#card').append(템플릿)
 })
@@ -49,7 +49,7 @@ products.forEach((a, i)=>{
   `<div class="col-sm-4">
     <img src="https://via.placeholder.com/600" class="w-100">
     <h5>${products[i].title}</h5>
-    <p>가격 : ${products[i].price}</p>
+    <p style = "margin-right : 5px" class="수평정렬 cost_btn">가격 : ${products[i].price}</p><div class="수평정렬 btn btn-danger buy_btn">구매</div>
   </div>`;
   $('#card').append(템플릿)
 })
@@ -70,7 +70,7 @@ products2.forEach((a, i)=>{
   `<div class="col-sm-4">
     <img src="https://via.placeholder.com/600" class="w-100">
     <h5>${a.title}</h5>
-    <p>가격 : ${a.price}</p>
+    <p style = "margin-right : 5px" class="수평정렬 cost_btn">가격 : ${products[i].price}</p><div class="수평정렬 btn btn-danger buy_btn">구매</div>
   </div>`;
   $('#card').append(템플릿)
 })
@@ -110,7 +110,7 @@ var list_making = function (y) {
                     `<div class="col-sm-4">
                         <img src="https://via.placeholder.com/600" class="w-100">
                         <h5>${a.title}</h5> 
-                        <p>가격 : ${a.price}</p> 
+                        <p style = "margin-right : 5px" class="수평정렬 cost_btn" >가격 : ${a.price}</p><div class="수평정렬 btn btn-danger buy_btn">구매</div>
                     </div>`;
                 $('#card').append(템플릿);
             });
@@ -132,7 +132,43 @@ if(clicker == 1){
 };   
 });
 
+ 
+$('.buy_btn').click(function(){
+  var thissell = $(this);
+  var 가격 = thissell.prev();
+  var 상품명 = 가격.prev();
+  console.log(가격.html());
+  console.log(상품명.html());
 
+  // 로컬 스토리지에서 기존 장바구니 목록 불러오기
+  var 기존목록 = localStorage.getItem('장바구니');
+  var 장바구니 = 기존목록 ? JSON.parse(기존목록) : [];
+
+  // 새 상품 추가하기
+  장바구니.push([가격.html(), 상품명.html()]);
+
+  // 업데이트된 목록 저장하기
+  localStorage.setItem('장바구니', JSON.stringify(장바구니));
+  
+})
+
+
+
+var 담은목록 = localStorage.getItem('장바구니');
+담은목록 = JSON.parse(담은목록);
+console.log(담은목록);
+
+console.log(담은목록.length)
+
+//어레이 1번 꺼내서 상품명 : 1번쨰 그다음 2번쨰(가격: 포함됨) 반복문 ... 
+
+ 
+for(i=0; i<담은목록.length; i++){
+  
+var 장바구니목록 = `<h3>상품명 : ${담은목록[i][1]}</h3> <h3>${담은목록[i][0]}</h3>`
+$('.jangbasket').append(장바구니목록)
+
+}
     
 
     
